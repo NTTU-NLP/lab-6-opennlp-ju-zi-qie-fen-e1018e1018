3a22c513005e89efa5b982869463b3a8ae4467d4
e1018e1018 <e1018e1018@gmail.com>
2018-04-09 13:52:00 +0800

    交作業

---
+++ Lab6/Lab6/Lab6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using opennlp.tools.sentdetect;
+using opennlp.tools.tokenize;
+
+namespace Lab6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DirectoryInfo folder = new DirectoryInfo(@"..\..\..\..\..\Dataset");
+            foreach (var fname in folder.GetFiles())
+            {
+                String line = File.ReadAllText(fname.FullName);
+                java.io.InputStream modelIn = new java.io.FileInputStream(@"..\..\..\..\..\en-sent.bin");
+                SentenceModel smodel = new SentenceModel(modelIn);
+                SentenceDetector detector = new SentenceDetectorME(smodel);
+                string[] sents = detector.sentDetect(line);
+                using (StreamWriter sw = new StreamWriter(fname.FullName.Replace(fname.FullName.Substring(fname.FullName.Length - 3), "rtf")))
+                {
+                    foreach (var sent in sents)
+                    {
+                        sw.WriteLine(sent);
+                    }
+                }
+            }
+        }
+    }
+}
